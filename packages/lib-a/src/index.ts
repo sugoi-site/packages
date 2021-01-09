3ab26fc18d11a24869af153c89c9cf983c7f4e62
shuntaka9576 <shuntaka9576@gmail.com>
2021-01-09 18:06:41 +0900

    feat(lib-a): add sampleB func

--- packages/lib-a/src/index.ts
+++ packages/lib-a/src/index.ts
@@ -2,6 +2,10 @@ export const sampleA = (): void => {
   console.log("sampleA");
 };
 
+export const sampleB = (): void => {
+  console.log("sampleB");
+};
+
 export const backendUtilFunc = (): void => {
   console.log("backendUtilFunc");
 };
